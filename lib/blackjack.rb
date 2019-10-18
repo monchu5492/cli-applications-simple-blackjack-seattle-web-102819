d2c28d25b17783485a1607491d429313fa142340
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-18 00:26:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,67 @@
+require 'pry'
+
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+      card = rand(1..11)
+      return card
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+   card1 = deal_card
+   card2 = deal_card
+   card_total = card1 + card2
+   display_card_total(card_total)
+   return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(number)
+  prompt_user
+  input = get_user_input
+  if input == 's'
+    return number
+  elsif input == 'h'
+    return deal_card + number
+  else 
+    invalid_command
+    hit?(number)
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+   puts "Please enter a valid command"
 end
-
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+welcome
+initial_round
+hit?(initial_round)
+display_card_total(hit?)
+  while 
+  hit? <= 21
+  end_game
+ end 
 end
     
